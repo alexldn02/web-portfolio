7f04a1cc12a9609f11f5ff4b5997cbf0c9f4f623
alexldn02 <alex99cairns@gmail.com>
2026-02-15 19:56:51 +0000

    second commit

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
-var audio = $("#mySoundClip")[0];
-audio.volume = 0.2; // Set the volume to 50%
+var audio = $("#tapeDispenser")[0];
+audio.volume = 0.4;
+audio.currentTime = 10;
 document.getElementById("myMusic").addEventListener("mouseenter", function() {
     audio.play();
 });
@@ -8,10 +9,31 @@ document.getElementById("myMusic").addEventListener("mouseleave", function() {
 });
 
 var audio2 = $("#synth")[0];
-audio2.volume = 0.2; // Set the volume to 50%
+audio2.volume = 0.4;
+audio2.currentTime = 0;
 document.getElementById("software").addEventListener("mouseenter", function() {
     audio2.play();
 });
 document.getElementById("software").addEventListener("mouseleave", function() {
     audio2.pause();
+});
+
+var audio3 = $("#demo")[0];
+audio3.volume = 0.5;
+audio3.currentTime = 90;
+document.getElementById("demos").addEventListener("mouseenter", function() {
+    audio3.play();
+});
+document.getElementById("demos").addEventListener("mouseleave", function() {
+    audio3.pause();
+});
+
+var audio4 = $("#typing")[0];
+audio4.volume = 0.4;
+audio4.currentTime = 0;
+document.getElementById("about").addEventListener("mouseenter", function() {
+    audio4.play();
+});
+document.getElementById("about").addEventListener("mouseleave", function() {
+    audio4.pause();
 });
